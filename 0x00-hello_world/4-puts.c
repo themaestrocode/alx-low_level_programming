ab8e5d22b1602e04031a68b4932fcf444bf4d367
victor <victorsoderu@gamil.com>
2023-02-16 09:12:32 -0800

    made a correction again to 4-puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -5,7 +5,8 @@
  *
  * Return: Always 0 (Success)
  */
-int main(void) {
-	puts("\"Programming is like building a multilingual puzzle");
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle\n");
 	return (0);
 }
