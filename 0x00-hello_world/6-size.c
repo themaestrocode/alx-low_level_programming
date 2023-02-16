72ca501f0c4c31031c93b550e734bc8970c42bcb
victor <victorsoderu@gamil.com>
2023-02-16 09:46:49 -0800

    new commit

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -3,7 +3,7 @@
 /**
  * main - Entry point
  *
- *this function prints the size of various types on the computer it is compiled and run on
+ *prints the size of various types on the computer it is compiled and run on
  *
  *Return: Always 0 (success)
  */
