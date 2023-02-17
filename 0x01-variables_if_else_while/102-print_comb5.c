fa1cd0bd9fb0fe94a5838c737e99d71fd13e002b
victor <victorsoderu@gamil.com>
2023-02-17 08:20:59 -0800

    final task completed (-_*)

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -9,4 +9,26 @@
  */
 int main(void)
 {
+	int i, j;
 
+	for (i = 0; i < 100; i++)
+	{
+		for (j = i + 1; j < 100; j++)
+		{
+			putchar(i / 10 + '0');
+			putchar(i % 10 + '0');
+			putchar(' ');
+			putchar(j / 10 + '0');
+			putchar(j % 10 + '0');
+
+			if (i < 99 || j < 99)
+			{
+				putchar(',');
+				putchar(' ');
+			}
+		}
+	}
+	putchar('\n');
+
+	return (0);
+}
