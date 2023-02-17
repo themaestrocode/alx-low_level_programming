1e613915bbb8d03af38589e58bc9db9636830b4c
victor <victorsoderu@gamil.com>
2023-02-17 06:29:24 -0800

    added 100-print_comb3.c

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Description: prints numbers in pairs
+ *
+ * Return: 0 (success)
+ */
+int main(void)
+{
+	int i, j;
+
+	for (i = 0; i < 10; i++)
+	{
+		for (j = 0; j < 10; j++)
+		{
+			if (i == j || i > j)
+				continue;
+
+			putchar(i + '0');
+			putchar(j + '0');
+
+			if (i == 8 && j == 9)
+				break;
+
+			putchar(',');
+			putchar(' ');
+		}
+	}
+
+	return (0);
+}
