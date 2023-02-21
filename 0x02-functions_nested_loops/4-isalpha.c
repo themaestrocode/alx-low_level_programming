f6e3f97250ee4189b8754f250cad37040d04914a
victor <victorsoderu@gamil.com>
2023-02-21 09:26:49 -0800

    added task 4-isalpha.c

---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,17 @@
+#include <ctype.h>
+#include "main.h"
+
+/**
+ * _isalpha - checks if an input is a charcter or not
+ *
+ * @c: the character to check
+ *
+ * Return: (1) (success), (0) (failure)
+ *
+ */
+int _isalpha(int c)
+{
+	if (isalpha(c))
+		return (1);
+	else
+		return (0);
