466125ac6f74f9b8b38d71dcc37fedd764cae76b
victor <victorsoderu@gamil.com>
2023-02-21 08:30:07 -0800

    added task 2

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * print_alphabet_x10 - prints the alphabets X10
+ *
+ */
+void print_alphabet_x10(void)
+{
+	char c;
+	int i;
+
+	for (i = 0; i < 10; i++)
+		print_alphabet();
+}
