53215a0b0013c3e2ce58f9528c5a22f708141ed6
victor <victorsoderu@gamil.com>
2023-02-21 03:13:11 -0800

    commited the first task

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Description - prints "_putchar"
+ *
+ * Return: 0 (success)
+ */
+int main(void)
+{
+	char x = '_', p = 'p', u = 'u', t = 't';
+	char c = 'c', h = 'h', a = 'a', r = 'r', y = '\n';
+
+	putchar(x);
+	putchar(p);
+	putchar(u);
+	putchar(t);
+	putchar(c);
+	putchar(h);
+	putchar(a);
+	putchar(r);
+	putchar(y);
+
+	return (0);
+}
---
+++ 0x02-functions_nested_loops/README.md
@@ -0,0 +1,1 @@
+This directory contains tasks of the 0x02. C - Functions, nested loops project
