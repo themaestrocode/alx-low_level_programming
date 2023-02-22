ed65c4fe83ce3dcacf6da9335739de6e82b1dba2
victor <victorsoderu@gamil.com>
2023-02-21 16:04:13 -0800

    pushed the fibbionacci

---
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+/**
+ * main- entry point
+ * Return: 0 (success)
+ *
+ */
+int main(void)
+{
+	int i, fib1 = 1, fib2 = 2, nextfib;
+
+	printf("%d, %d", fib1, fib2);
+
+	for (i = 3, i <= 50; i++)
+	{
+		nextfib = fib1 + fib2;
+		printf(", %d", nextfib);
+		fib1 = fib2;
+		fib2 = nextfib;
+	}
+	printf("\n");
+
+	return (0);
+}
