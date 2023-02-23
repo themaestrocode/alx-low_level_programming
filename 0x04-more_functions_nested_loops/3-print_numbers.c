42d6d027569e759523327fe48346d995a3d1657b
victor <victorsoderu@gamil.com>
2023-02-23 06:21:36 -0800

    added 3-print_numbers.c

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * print_numbers - prints integers from 0 - 9, then a new line
+ */
+void print_numbers(void)
+{
+	int i;
+
+	while (i < 10)
+	{
+		_putchar(i + '0');
+		i++;
+	}
+
+	_putchar('\n');
+}
