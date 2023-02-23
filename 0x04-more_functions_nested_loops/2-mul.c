abfd6f3cf112615a11b881aa7805684bfc121766
victor <victorsoderu@gamil.com>
2023-02-23 06:16:05 -0800

    added 2-mul.c

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * mul -finds the product of two ints
+ * @a:integer 1
+ * @b: integer 2
+ * Return: prod (success)
+ */
+int mul(int a, int b)
+{
+	int prod;
+
+	prod = a * b;
+	return (prod);
+}
