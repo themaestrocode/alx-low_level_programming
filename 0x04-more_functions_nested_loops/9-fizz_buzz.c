7714dce18a56c8058c168e3491b606ef146496b8
victor <victorsoderu@gamil.com>
2023-02-23 15:53:36 -0800

    added 9-fizz_buzz.c

---
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point to the FizzBuzz problem
+ * Return: 0 (success)
+ */
+int main(void)
+{
+	int counter, three, five;
+
+	for (counter = 1; counter <= 100; counter++)
+	{
+		three = counter % 3;
+		five = counter % 5;
+
+		if (three == 0 && five == 0)
+		{
+			printf("FizzBuzz ");
+			continue;
+		}
+
+		if (three == 0)
+		{
+			printf("Fizz ");
+			continue;
+		}
+
+		if (five == 0)
+		{
+			printf("Buzz ");
+			continue;
+		}
+
+		printf("%d ", counter);
+	}
+}
