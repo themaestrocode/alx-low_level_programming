3f13a811b3f4a2606b120770d4e1e0e4bccb3001
victor <victorsoderu@gamil.com>
2023-02-23 13:01:33 -0800

    added 7-print_diagonal

---
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -0,0 +1,31 @@
+#include "main.h"
+
+/**
+ * print_diagonal - prints the \ character n times
+ * @n: number oftimes the \ charcater is to be printed
+ */
+void print_diagonal(int n)
+{
+	int i;
+
+	if (n > 0)
+	{
+		for (i = 0; i < n; i++)
+		{
+			int j;
+
+			if (i > 0)
+			{
+				for (j = 0; j < i; j++)
+					_putchar(' ');
+			}
+
+			_putchar('\\');
+			_putchar('\n');
+		}
+
+		_putchar('\n');
+	}
+	else
+		_putchar('\n');
+}
