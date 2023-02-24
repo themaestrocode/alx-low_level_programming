1d8fbfc91b3ae81d56c914dc7dc4facc927f5665
victor <victorsoderu@gamil.com>
2023-02-23 18:41:44 -0800

    added 102

---
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * print_number - prints an integer
+ * @n: the integer value to be printed
+ */
+void print_number(int n)
+{
+	if (n < 0)
+	{
+		_putchar('-');
+		n = -n;
+	}
+
+	if (n / 10)
+	{
+		print_number(n / 10);
+	}
+
+	putchar(n % 10 + '0');
+}
