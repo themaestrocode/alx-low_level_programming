8d3855f6d007fd896ee7d837339d14423e2565a1
victor <victorsoderu@gamil.com>
2023-03-02 02:14:58 -0800

    added 1

---
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+ * _strncat - concatenates n-bytes of src to dest
+ * @dest: destination
+ * @src: source
+ * Return: dest (success)
+ */
+char *_strncat(char *dest, char *src, int n)
+{
+	int i = 0, j = 0;
+
+	while (dest[i] != '\0')
+	{
+		i++;
+	}
+
+	while (src[j] < n)
+	{
+		dest[i] = src[j];
+		i++;
+		j++;
+	}
+	dest[i] = '\0';
+	return (dest);
+}
