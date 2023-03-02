da6fc6918af13926de3b997f3ce8916eae93559b
victor <victorsoderu@gamil.com>
2023-03-02 02:02:46 -0800

    new commit

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -11,13 +11,12 @@ char *_strcat(char *dest, char *src)
 	int i, j;
 
 	for (i = 0; dest[i] != '\0'; i++)
+
+	for (j = 0; src[j] != '\0'; j++)
 	{
-		for (j = 0; src[j] != '\0'; j++)
-		{
-			dest[i + j] = src[j];
-		}
+		dest[i + j] = src[j];
 	}
 
 	dest[i + j] = '\0';
-	return (*dest);
+	return (dest);
 }
