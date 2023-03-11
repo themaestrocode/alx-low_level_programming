2710c3628c63f97961bde3b296842e1e5224ea2f
victor <victorsoderu@gamil.com>
2023-03-10 19:10:25 -0800

    new correction

---
+++ 0x09-static_libraries/main.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * main - does something
+ * Return: 0 (success)
+ */
+int main(void)
+{
+	_puts("\"At the end of the day, my goal was to be the best hacker
+			\"\n\t- Kevin Mitnick");
+	return (0);
+}
+
