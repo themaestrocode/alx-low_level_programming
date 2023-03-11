e187ba684e696c09a09a31b51317afa8b15ee37c
victor <victorsoderu@gamil.com>
2023-03-11 08:22:59 -0800

    new commit

--- 0x09-static_libraries/create_static_lib.sh
+++ 0x09-static_libraries/create_static_lib.sh
@@ -1,7 +1,3 @@
 #!/bin/bash
-
-# Extract object files from  all c files in current directory.
 gcc -c *.c
-
-# Create a static library called liball.a from all newly created object files.
 ar rcs liball.a *.o
