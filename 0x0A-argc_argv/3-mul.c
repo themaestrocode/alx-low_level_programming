40074483eea53268051fca8146e56f78fc873754
victor <victorsoderu@gamil.com>
2023-03-11 14:05:24 -0800

    added 3-mul.c

---
+++ 0x0A-argc_argv/3-mul.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ * @argc: argumemnt count
+ * @argv: argument string array
+ * Return: 0 (success)
+ */
+int main(int argc, char *argv[])
+{
+	(void) argv;
+
+	int prod;
+
+	if (argc != 3)
+	{
+		printf("Error\n");
+		return (1);
+	}
+
+	prod = atoi(argv[1]) * atoi(argv[2]);
+	printf("%d\n", prod);
+
+	return (0);
+}
