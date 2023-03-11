5621f4edff65eb4bf2229e7e6eacd59add4cc1cf
victor <victorsoderu@gamil.com>
2023-03-11 08:09:04 -0800

    fone

--- 0x09-static_libraries/create_static_lib.sh
+++ 0x09-static_libraries/create_static_lib.sh
@@ -1,10 +1,7 @@
 #!/bin/bash
 
 # Extract object files from  all c files in current directory.
-gcc -Wall -Werror -Wextra -pedantic -std=gnu89 -c *.c
+gcc -c *.c
 
 # Create a static library called liball.a from all newly created object files.
 ar rcs liball.a *.o
-
-# Clean up the object files
-rm *.o
