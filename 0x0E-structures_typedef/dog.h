adb50c2ff5b058c6a2d2d0cbab66d1e4e2a3ab81
victor <victorsoderu@gamil.com>
2023-03-21 07:09:49 -0700

    added dog.h

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,17 @@
+#ifndef DOG_H
+#define DOG_H
+
+/**
+ * struct dog - basic structure of a dog
+ * @name: name of the dog
+ * @age: age of the dog
+ * @owner: the dog's owner
+ */
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+
+#endif /* DOG_H */
---
+++ 0x0E-structures_typedef/git_push.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+git add .
+
+echo "Enter your commit message: "
+
+read commit_message
+
+git commit -m "$commit_message"
+
+git push
+
+echo -e "\ngit push successful"
+
+echo -e "Your commit message: $commit_message\n"
