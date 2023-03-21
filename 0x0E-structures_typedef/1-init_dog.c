c8c37ae7ea1f5ef8b30494e56c3126553aaf2747
victor <victorsoderu@gamil.com>
2023-03-21 07:20:07 -0700

    adde 1-inti_dog.c

---
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -0,0 +1,18 @@
+#include "dog.h"
+
+/**
+ * int_dog - initializes a dog struct
+ * @d: pointer to the dog struct
+ * @name: pointer to the dog's name
+ * @age: pointer to the dog's age
+ * @owner: pointer to the dog's owner
+ */
+void init_dog(struct dog *d, char *name, float age, char *owner)
+{
+	if (d)
+	{
+		d->name = name;
+		d->age = age;
+		d->owner = owner;
+	}
+}
--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -14,4 +14,10 @@ struct dog
 	char *owner;
 };
 
+
+int _putchar(char);
+void init_dog(struct dog *d, char *name, float age, char *owner);
+void print_dog(struct dog *d);
+dog_t *new_dog(char *name, float age, char *owner);
+
 #endif /* DOG_H */
