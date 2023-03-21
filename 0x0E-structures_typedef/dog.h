ad25df43372bdeb04cb3537478830250341be0dd
victor <victorsoderu@gamil.com>
2023-03-21 07:46:52 -0700

    new correctrion

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -14,10 +14,15 @@ struct dog
 	char *owner;
 };
 
+/**
+ * dog_t - Typedef for struct dog
+ */
+typedef struct dog dog_t;
 
 int _putchar(char);
 void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
 dog_t *new_dog(char *name, float age, char *owner);
+void free_dog(dog_t *d);
 
 #endif /* DOG_H */
