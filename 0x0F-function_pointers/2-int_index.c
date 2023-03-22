89e95fdf240ec8614046a5d2ebb9266d4b3305a4
victor <victorsoderu@gamil.com>
2023-03-22 08:35:45 -0700

    fixed 2

--- 0x0F-function_pointers/2-int_index.c
+++ 0x0F-function_pointers/2-int_index.c
@@ -2,10 +2,11 @@
 #include <stdlib.h>
 
 /**
- * int_index - return index place if comparison = true, else -1
+ * int_index - return index place if comparison = true, else (-1)
  * @array: array
  * @size: size of elements in array
  * @cmp: pointer to func of one of the 3 in main
+ * Return: i, otherwise -1
  */
 int int_index(int *array, int size, int (*cmp)(int))
 {
