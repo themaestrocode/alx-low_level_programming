1d5f41fafa77c02bda025f2ed847ed64b73028d6
victor <victorsoderu@gamil.com>
2023-03-22 07:54:45 -0700

    new comit

--- 0x0F-function_pointers/0-print_name.c
+++ 0x0F-function_pointers/0-print_name.c
@@ -1,4 +1,5 @@
 #include "function_pointers.h"
+#include <stdlib.h>
 
 /**
  * print_name - prints name
@@ -7,5 +8,5 @@
  */
 void print_name(char *name, void (*f)(char *))
 {
-	(*f)(name);
+	f(name);
 }
