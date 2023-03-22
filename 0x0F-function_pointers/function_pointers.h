0980b755ee8856445d7c4c4043ca9e8a85f41a96
victor <victorsoderu@gamil.com>
2023-03-22 06:13:48 -0700

    correction to the header file

--- 0x0F-function_pointers/function_pointers.h
+++ 0x0F-function_pointers/function_pointers.h
@@ -1,6 +1,8 @@
 #ifndef FUNCTIONS_POINTERS_H
 #define FUNCTIONS_POINTERS_H
 
+#include <stdlib.h>
+
 int _putchar(char c);
 void print_name(char *name, void (*f)(char *));
 void array_iterator(int *array, size_t size, void (*action)(int));
