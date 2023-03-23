b95acd23226de09b2a5d9c62990753d18d142619
victor <victorsoderu@gamil.com>
2023-03-23 10:43:24 -0700

    testing

--- 0x10-variadic_functions/git_push.sh
+++ 0x10-variadic_functions/git_push.sh
@@ -1,16 +1,28 @@
 #!/bin/bash
 git add .
 
-
+if [ $? -eq 0 ]; then
+	echo -e "git add command successful\n"
+else
+	echo -e "git add command failed\n"
+fi
 
 echo "Enter your commit message: "
-
 read commit_message
 
 git commit -m "$commit_message"
 
+if [ $? -eq 0 ]; then
+	echo -e "git commit successful\n"
+else
+	echo -e "git commit failed\n"
+fi
+
 git push
 
-echo -e "\ngit push successful"
-
-echo -e "Your commit message: $commit_message\n"
+if [ $? -eq 0 ]; then
+	echo -e "\ngit push successful\n"
+	echo -e "Your commit message: $commit_message\n"
+else
+	echo -e "git push failed\n"
+fi
