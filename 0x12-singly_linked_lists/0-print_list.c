59c00e4421a31d2b43a97b1f66da2f464944cd7c
victor <victorsoderu@gamil.com>
2023-03-30 08:33:33 -0700

    added task 0

---
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -0,0 +1,26 @@
+#include "lists.h"
+#include <stdlib.h>
+
+/**
+ * print_list - prints all the elements of a list_t list
+ * @h: pointr to the head list
+ * Return: i (the counter)
+ */
+size_t print_list(const list_t *h)
+{
+	size_t i = 0;
+
+	while (h != NULL)
+	{
+		printf("[%u] ", h->len);
+
+		if (h->str == NULL)
+			printf("(nil)\n");
+		else
+			printf("%s\n", h->str);
+
+		h = h->next;
+		i++;
+	}
+	return (i);
+}
