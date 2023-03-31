66426c96b14d7963b3bf3dec8798804ff71e49dd
victor <victorsoderu@gamil.com>
2023-03-31 04:53:22 -0700

    added task 4

---
+++ 0x12-singly_linked_lists/4-free_list.c
@@ -0,0 +1,20 @@
+#include "lists.h"
+#include <stdlib.h>
+
+/**
+ * free_list - frees a list_t list
+ * @head: pointer to the head of the list
+ */
+void free_list(list_t *head)
+{
+	list_t *current, *next;
+
+	current = head;
+	while (current != NULL)
+	{
+		next = current->next;
+		free(current->str);
+		free(current);
+		current = next;
+	}
+}
