cb7953090efa6447776f603a5433e895f7bbbb2a
victor <victorsoderu@gamil.com>
2023-03-31 05:31:17 -0700

    added task 6

---
+++ 0x12-singly_linked_lists/101-hello_holberton.asm
@@ -0,0 +1,19 @@
+section .data
+	msg db 'Hello, Holberton', 0Ah	; the string to print
+	format db '%s', 0Ah		; the format string for printf
+
+section .text
+	global main
+
+main:
+	push rbp			; save the base pointer
+	mov rbp, rsp			; set the base pointer to the current stack pointer
+
+	lea rdi, [msg]			; load the address of the hello string into rdi
+	mov eax, 0			; set eax to 0
+	call printf			; call the printf function
+
+	mov rsp, rbp			; restore the stack pointer
+	pop rbp				; restore the base pointer
+	xor eax, eax			; set the return value to 0
+	ret				; return to the operating system
