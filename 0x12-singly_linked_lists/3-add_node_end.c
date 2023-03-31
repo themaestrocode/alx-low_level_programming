55883388a2903ad04b7c22eb44a2cbf62f960cf4
victor <victorsoderu@gamil.com>
2023-03-31 03:19:48 -0700

    fixee task 3

--- 0x12-singly_linked_lists/3-add_node_end.c
+++ 0x12-singly_linked_lists/3-add_node_end.c
@@ -10,7 +10,7 @@
  */
 list_t *add_node_end(list_t **head, const char *str)
 {
-	list_t *new_node, *current;;
+	list_t *new_node, *current;
 
 	new_node = malloc(sizeof(list_t));
 	if (new_node == NULL)
@@ -25,7 +25,6 @@ list_t *add_node_end(list_t **head, const char *str)
 
 	new_node->len = strlen(str);
 	new_node->next = NULL;
-	
 	if (*head == NULL)
 	{
 		*head = new_node;
