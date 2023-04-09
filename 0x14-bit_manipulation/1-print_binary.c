318bad97143be8d06d8a0048340fed20a4cfb6a3
victor <victorsoderu@gamil.com>
2023-04-09 06:17:19 -0700

    added task 1.1

---
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+/**
+ * print_binary - prints the binary rep of an integer
+ * @n: the integer to be converted to binary
+ */
+void print_binary(unsigned long int n)
+{
+	unsigned long int mask;
+	int bit_count;
+
+	mask = 1ul << (sizeof(unsigned long int) * 8 - 1);
+	bit_count = sizeof(unsigned long int) * 8;
+
+	while (bit_count > 0)
+	{
+		if ((n & mask) != 0)
+			printf("1");
+		else
+			printf("0");
+
+		mask >>= 1;
+		bit_count--;
+	}
+}
