1364c159840f62ada171cdcb2bb7f82f58cc6359
victor <victorsoderu@gamil.com>
2023-04-11 08:33:30 -0700

    new commit

--- 0x14-bit_manipulation/1-print_binary.c
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -1,28 +1,26 @@
 #include "main.h"
 
 /**
- * print_binary - prints the binary rep of an integer
- * @n: the integer to be converted to binary
+ * print_binary - prints the binary equivalent of a decimal number
+ * @n: number to print in binary
  */
 void print_binary(unsigned long int n)
 {
-	unsigned long int crnt;
-	int i, bit_count = 0;
+	int i, count = 0;
+	unsigned long int current;
 
 	for (i = 63; i >= 0; i--)
 	{
-		crnt = n >> 1;
+		current = n >> i;
 
-		if (crnt & 1)
+		if (current & 1)
 		{
 			_putchar('1');
-			bit_count++;
+			count++;
 		}
-		else if (bit_count)
-		{
+		else if (count)
 			_putchar('0');
-		}
 	}
-	if (!bit_count)
+	if (!count)
 		_putchar('0');
 }
