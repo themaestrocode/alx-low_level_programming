9fdb79e38951142487d875b4f36382d38d274930
victor <victorsoderu@gamil.com>
2023-04-16 10:44:24 -0700

    final correction to task 1

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -9,7 +9,7 @@
  */
 int create_file(const char *filename, char *text_content)
 {
-	int fd, len = strlen(text_content), writeB;
+	int fd, len = 0, writeB;
 
 	if (filename == NULL)
 		return (-1);
@@ -20,8 +20,11 @@ int create_file(const char *filename, char *text_content)
 
 	if (text_content != NULL)
 	{
+		while (text_content[len])
+			len++;
+
 		writeB = write(fd, text_content, len);
-		if (writeB != len)
+		if (writeB == -1)
 		{
 			close(fd);
 			return (-1);
